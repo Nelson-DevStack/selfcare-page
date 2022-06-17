677b1ba7ddaca553c25995b47e7cf6c8b821d4c8
Nelson C <n.devpro@protonmail.com>
2022-06-17 15:56:01 -0400

    navbar

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,33 @@
+import style from './style.module.css';
+import { BsStarFill, BsSearch, BsCart2 } from 'react-icons/bs';
+import { BiUser } from 'react-icons/bi';
+
+export const Navbar = () => {
+  return (
+    <header className={style.navbar}>
+      <div className={style.navbarContainer}>
+        <a className={style.navbarBrand}>
+          <BsStarFill />
+          selfcare
+        </a>
+        <nav className={style.navbarLinks}>
+          <li className={style.navItem}>
+            <a>
+              <BsSearch/>
+            </a>
+          </li>
+          <li className={style.navItem}>
+            <a>
+              <BiUser />
+            </a>
+          </li>
+          <li className={style.navItem}>
+            <a>
+              <BsCart2/>
+            </a>
+          </li>
+        </nav>
+      </div>
+    </header>
+  )
+};
