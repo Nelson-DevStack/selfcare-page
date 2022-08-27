ac3448ea6b0209626978dc4382478c2e0cabc704
Nelson C <n.devpro@protonmail.com>
2022-08-26 20:59:06 -0400

    Swiper navigation and pagination

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import styles from './style.module.css';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
+import { Navigation, Autoplay, Pagination } from 'swiper';
 import { Products } from '../../data/Products';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 
 const Slider = () => {
   return (
     <div className={styles.sliderWrapper}>
-      <Swiper navigation={true} modules={[Navigation]}>
+      <Swiper
+        navigation={true}
+        modules={[Navigation, Autoplay, Pagination]}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 4000 }}
+        loop={true}
+      >
         {Products.map((product) => (
           <SwiperSlide key={product.id}>
             <div className={styles.imageWrapper}>
